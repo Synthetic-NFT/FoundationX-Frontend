bb4e3b1104301d2d8984017b1263899d28b703e3
Zhili Feng <fzl.007@gmail.com>
2022-03-18 16:02:46 -0400

    metamask connection

--- frontend/src/TopBar.tsx
+++ frontend/src/TopBar.tsx
@@ -3,11 +3,15 @@ import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import CircularProgress from "@mui/material/CircularProgress";
-import React, { useContext, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 import api from "./api";
 import { AppContext } from "./AppContext";
 import theme from "./theme";
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+} from "./util/interact";
 
 const useStyles = makeStyles({
   root: {
@@ -21,41 +25,90 @@ const useStyles = makeStyles({
 export default function TopBar(): React.ReactElement {
   const { appData, setAppData } = useContext(AppContext);
   const [loading, setLoading] = useState(false);
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
   const styles = useStyles();
+
+  function addWalletListener() {
+    if ((window as any).ethereum) {
+      (window as any).ethereum.on("accountsChanged", (accounts: string | any[]) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus("Not installed"      );
+    }
+  }
+
+  useEffect(() => {
+    const setWalletAndStatus = async () => {
+      const { address, status } = await getCurrentWalletConnected();
+      setWallet(address);
+      setStatus(status);
+    }
+    setWalletAndStatus();
+    addWalletListener();
+  }, []);
+
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+
+
   return (
-    <AppBar position="static">
-      <Toolbar className={styles.root}>
-        <div className={styles.spacer} />
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <Button
-            color="inherit"
-            onClick={() => {
-              // we don't want to send duplicated request for connecting wallet.
-              if (loading) {
-                return;
-              }
-              setLoading(true);
-
-              // disconnect
-              if (appData != null) {
-                setAppData(null);
-                api.disconnect().then(() => setLoading(false));
-                return;
-              }
-
-              // connect
-              api.connect().then((newAppData) => {
-                setLoading(false);
-                setAppData(newAppData);
-              });
-            }}
-          >
-            {appData?.userName ?? "Not Connected"}
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+      <AppBar position="static">
+        <Toolbar className={styles.root}>
+          <div className={styles.spacer} />
+          {loading ? (
+              <CircularProgress />
+          ) : (
+              // <Button
+              //   color="inherit"
+              //   onClick={() => {
+              //     // we don't want to send duplicated request for connecting wallet.
+              //     if (loading) {
+              //       return;
+              //     }
+              //     setLoading(true);
+              //
+              //     // disconnect
+              //     if (appData != null) {
+              //       setAppData(null);
+              //       api.disconnect().then(() => setLoading(false));
+              //       return;
+              //     }
+              //
+              //     // connect
+              //     api.connect().then((newAppData) => {
+              //       setLoading(false);
+              //       setAppData(newAppData);
+              //     });
+              //   }}
+              // >
+              //   {appData?.userName ?? "Not Connected"}
+              // </Button>
+              <Button color="inherit" id="walletButton" onClick={connectWalletPressed}>
+                {walletAddress.length > 0 ? (
+                    `Connected: ${ 
+                    String(walletAddress).substring(0, 6) 
+                    }...${ 
+                    String(walletAddress).substring(38)}`
+                ) : (
+                    <span>Connect Wallet</span>
+                )}
+              </Button>
+          )}
+        </Toolbar>
+      </AppBar>
   );
 }
