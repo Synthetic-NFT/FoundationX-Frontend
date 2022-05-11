0f289406bfc5a58a200f9ec62d74e168c445aa71
Zhili Feng <fzl.007@gmail.com>
2022-05-11 01:53:08 -0400

    use vault for contract

---
+++ frontend/src/util/ContractAddress.ts
@@ -0,0 +1,15 @@
+const ContractAddress = {
+    factory: "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82",
+    oracle: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+    tokens : [
+        {
+            name: "BoredApeYachtClub",
+            synth: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+            vault: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+            reserve: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+        },
+    ]
+}
+
+export default ContractAddress;
+
